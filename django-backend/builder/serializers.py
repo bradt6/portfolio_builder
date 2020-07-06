91a8b4b7c4dc6af0372d8edd74bd0ae33ea855c8
Brad Thurlow <bradthurlow@Brads-MacBook-Pro.local>
2020-07-06 15:21:23 +1000

    building out the builder class

---
+++ django-backend/builder/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.reverse import reverse
+from rest_framework.serializers import ModelSerializer
+
+from .models import Builder, PageManager, Template
+
+
+class TemplateSerializer(ModelSerializer):
+    class Meta:
+        model = TemplateSerializer
+        fields = "__all__"
+class PageManagerSerializer(ModelSerializer):
+    class Meta:
+        model = PageManager
+        fields = "__all__"
+
+class BuilderSerializer(ModelSerializer):
+    page_manager = PageManagerSerializer(read_only=True)
+    template_serializer = TemplateSerializer(read_only=True)
+    class Meta:
+        model = Builder
+        fields = "__all__"
