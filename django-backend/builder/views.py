2514699b4458dbd976e8b7494cba5503916073d2
Brad Thurlow <bradthurlow@Brads-MacBook-Pro.local>
2020-07-16 16:40:08 +1000

    added template builder

--- django-backend/builder/views.py
+++ django-backend/builder/views.py
@@ -9,34 +9,24 @@ from django.shortcuts import get_object_or_404
 class HomePageView(TemplateView):
     template_name = "builder/template.html"
 
-    # model = Builder:w
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["builder"] = Builder.objects.all()
-        # context["template"] = Builder.objects.values('template')
-        template_id = Builder.objects.values('template')
-        template_value = template_id[0]['template']
-        template_name = Template.objects.get(id=template_value)
-        context["template"] = template_value
-        context["template_name"] = template_name.cdn_url_path
-        # context['builder'] = Builder.objects.filter(pk=self.kwargs['id'])
+
+    #    This get the template object
+        template_object = Builder.objects.values('template')
+        template_id = template_object[0]['template']
+        template_name = Template.objects.get(id=template_id)
+        context["template"] = template_name
+
+        # This get the pageManger object
+        pageManager_object = Builder.objects.values('pageManager')
+        pageManager_id = pageManager_object[0]['pageManager']
+        pageManger_name = PageManager.objects.get(id=pageManager_id)
+        context['pageManagerData'] = pageManger_name
+
         return context
-
-# class HomePageView(TemplateView):
-#     template_name = "builder/template.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["builder"] = Builder.objects.all()
-#         builder_id = self.kwargs['builder']
-#         builder = get_object_or_404(Builder, pk=builder_id)
-#         context['builder'] = builder
-#         return context
 
 class RetreiveAPIDetail(RetrieveAPIView):
     queryset = Builder.objects.all()
     serializer_class = BuilderSerializer
-
-# class indexView(IndexView):
-#     template_name = "builder/template.html"
