d1d31627923da010fd1c5e96924b8a221d133d28
Brad Thurlow <bradthurlow@Brads-MacBook-Pro.local>
2020-08-11 15:33:25 +1000

    added theme picker accross all templates

--- django-backend/builder/templatetags/nav_heading.py
+++ django-backend/builder/templatetags/nav_heading.py
@@ -8,12 +8,12 @@ def get_theme():
     template_object = Builder.objects.values('template')
     template_id = template_object = template_object[0]['template']
     template_name = Template.objects.get(id = template_id)
-    return template_name
+    return template_name.cdn_url_path
 
 @register.simple_tag
 def get_headings():
     pageManager_object = Builder.objects.values('pageManager')
-    pageManager_id = pageManager_objects[0]['pageManager']
+    pageManager_id = pageManager_object[0]['pageManager']
     pageManager_name = PageManager.objects.get(id=pageManager_id)
     return pageManager_name
     
