d512cd0baee25cd0b07d3390d865e7eb33dbebf2
Brad Thurlow <bradthurlow@Brads-MacBook-Pro.local>
2020-08-11 17:33:44 +1000

    menu is getting formed via a inclusion tag. hardcoded List

--- django-backend/builder/templatetags/nav_heading.py
+++ django-backend/builder/templatetags/nav_heading.py
@@ -15,5 +15,26 @@ def get_headings():
     pageManager_object = Builder.objects.values('pageManager')
     pageManager_id = pageManager_object[0]['pageManager']
     pageManager_name = PageManager.objects.get(id=pageManager_id)
+
     return pageManager_name
-    
+
+@register.inclusion_tag('builder/nav.html')
+def get_headings_inclusion_tags():
+    pageManager_object = Builder.objects.values('pageManager')
+    pageManager_id = pageManager_object[0]['pageManager']
+    pageManager_name = PageManager.objects.get(id=pageManager_id)
+
+    heading_list = []
+    if pageManager_name.home:
+        heading_list.append("Home")
+    if pageManager_name.portfolio:
+        heading_list.append("Portfolio")
+    if pageManager_name.services:
+        heading_list.append("Services")
+    if pageManager_name.contact:
+        heading_list.append("Contact")
+    if pageManager_name.about:
+        heading_list.append("About")
+
+    return {'heading_list' : heading_list }
+     
