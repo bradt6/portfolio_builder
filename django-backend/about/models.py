46933158eeb8b98cdedf57946063b0b1e0d6cfa9
Brad Thurlow <bradthurlow@gmail.com>
2020-09-14 18:51:48 +1000

    added home page binding

--- django-backend/about/models.py
+++ django-backend/about/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class AboutPage(models.Model):
 
-    name_of_bussiness = models.Charfield()
-    description = models.Textfield()
+    name_of_bussiness = models.CharField(max_length=27)
+    description = models.TextField()
     # This is ABN (AUS) || UK, Europe and USA.
-    business_number = models.Charfield()
+    business_number = models.CharField(max_length=27)
     #Location Services
-    lat = models.DecimalField(_('Latitude'), max_digits=10, decimal_places=8)
-    lng = models.DecimalField(_('Longitude'), max_digits=11, decimal_places=8)
+    lat = models.DecimalField(max_digits=10, decimal_places=8)
+    lng = models.DecimalField(max_digits=11, decimal_places=8)
 
