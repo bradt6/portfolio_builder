3acfa18da5b86b110995051845f358eecd4fce49
Brad Thurlow <bradthurlow@gmail.com>
2020-09-16 15:50:37 +1000

    editing the about template. trying to get dates and opening hours to work

--- django-backend/about/admin.py
+++ django-backend/about/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import AboutPage
+from .models import AboutPage, OpeningHours
 # Register your models here.
 
 @admin.register(AboutPage)
 class AboutAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(OpeningHours)
+class OpeningHoursAdmin(admin.ModelAdmin):
     pass
--- django-backend/about/views.py
+++ django-backend/about/views.py
@@ -8,7 +8,10 @@ class AboutPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # context['about_data'] = AboutPage.objects.all()[0]
+        about_object = AboutPage.objects.get(pk=1)
         context['about_data'] = AboutPage.objects.get(pk=1)
-        context['name'] =AboutPage.objects.get(pk=1).name_of_bussiness
+        context['name'] = about_object.name_of_bussiness
+        context['description'] = about_object.description
+        context['opening_hours'] = about_object.opening_times 
 
         return context
