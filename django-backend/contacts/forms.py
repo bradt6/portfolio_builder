c7b5c44ef823193c4b6a98b7816c3575170ab8fa
Brad Thurlow <bradthurlow@gmail.com>
2020-09-24 15:49:35 +1000

    added some basic bootsrap form to contact page

--- django-backend/contacts/forms.py
+++ django-backend/contacts/forms.py
@@ -2,6 +2,14 @@ from django.forms import ModelForm, Textarea
 from .models import Contact
 
 class ContactForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.attrs.get('class'):
+                field.widget.attrs['class'] += ' form-control'
+            else:
+                field.widget.attrs['class']='form-control'
     
     class Meta:
         model = Contact
