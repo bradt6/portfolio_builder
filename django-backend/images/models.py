c7dc209fcadf4fe97ddc501483f137227a76231f
Brad Thurlow <bradthurlow@gmail.com>
2020-12-09 13:29:40 +1100

    about to try adding inline methods

--- django-backend/images/models.py
+++ django-backend/images/models.py
@@ -13,8 +13,10 @@ def create_path(instance, filename):
 class Image(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=127)
-    # image_file = models.ImageField(upload_to="images")
-    image_file = models.ImageField(upload_to=create_path)
+
+    # image_file = models.ImageField(upload_to=create_path)
+    image_file = models.ImageField(upload_to="images/")
+
     # Could this be integrated with openCV for ai auto complete
     # web_alt = models.CharField(max_length=64)
     created_at = models.DateField(auto_now_add=True, db_index=True)
