28a26ba6713e36fa828a7bf2de6c4390ed2789b1
Brad Thurlow <bradthurlow@gmail.com>
2020-12-14 13:58:30 +1100

    currently building. trying to get the image gallery implemented in the portfolio section

--- django-backend/portfolio/admin.py
+++ django-backend/portfolio/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Portfolio
 
+from images.models import Image
 # admin.site.register(Portfolio)
+
+# class ImageInline(admin.TabularInline):
+#     model = Image
 
 @admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "get_images") 
+    list_display = ("name", "description", "get_images")
+    # inlines = [
+    #     ImageInline,
+    # ]
