3af5f8d9f2a77d0dc6de27f782130422e046f8ac
Brad Thurlow <bradthurlow@gmail.com>
2020-12-16 14:27:53 +1100

    Got the fixtures to work but it isnt working with the entrypoint docker file

--- django-backend/builder/management/commands/maybe_init_data.py
+++ django-backend/builder/management/commands/maybe_init_data.py
@@ -1,5 +1,5 @@
 from django.core.management import call_command
-from django.core.mangement.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from builder.models import Template
 
@@ -7,5 +7,7 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         if not Template.objects.exists():
-            self.stdout.write('Sending initital template data')
+            self.stdout.write('\n\n\n**** Sending initital template data ****\n\n\n')
             call_command('loaddata', 'builder/fixtures/templates.json')
+        else:
+            self.stdout.write('\n\n\n**** Inititial data already exists ****\n\n\n') 
